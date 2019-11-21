f4a9a35ecafdfc8cf9bbf3f71856c0e1a1f49ac6
Hayk Hakobyan <qyomjan@gmail.com>
2019-11-20 23:00:52 -0800

    Update complete_place_data.js

--- complete_place_data.js
+++ complete_place_data.js
@@ -4,9 +4,24 @@ const PlaceData=require('./place_data.js');
  * image url.
  */
 module.exports = class CompletePlaceData{
-    
+  // Note that I found composition more appropriate than inheritance as its more concise
   constructor(placeData, imgUrl = null) {
     this._placeData = placeData;
     this._imgUrl = imgUrl;
   }
 }
+
+/**
+Below is an example of an inheritance route:
+module.exports = class CompletePlaceData extends PlaceData{
+    
+  constructor(placeData, imgUrl = null) {
+    //console.log("placeData",placeData);
+    let vars;
+    ([...vars] = Object.values(placeData));
+    vars.splice(1,1,...Object.values(vars[1]));
+    super(...vars);
+    this._imgUrl = imgUrl;
+  }
+}
+**/
